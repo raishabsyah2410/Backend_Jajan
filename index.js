92398b783ee4dedf1f94be84d894d6582190f1ed
Hitech <raistamiang24@gmail.com>
2020-01-11 02:06:48 +0700

    backend Jajan

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("express-group-routes");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
